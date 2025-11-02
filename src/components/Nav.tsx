272c16780d873238d90b99090fc7da9f38f6018d
Lela Star <lelastar8888@gmail.com>
2025-11-02 12:22:06 -0500

    Refactor Nav component and update global styles for improved theming and layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,11 +2,13 @@ import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
   return (
-    <nav style={{ background: '#fff', padding: '10px 20px', borderBottom: '1px solid #ccc' }}>
-      <NavLink to="/" style={{ marginRight: 10 }}>Accueil</NavLink>
-      <NavLink to="/tableaudebord" style={{ marginRight: 10 }}>Tableau de bord</NavLink>
-      <NavLink to="/notesrapides" style={{ marginRight: 10 }}>Notes rapides</NavLink>
-      <NavLink to="/assistant">Assistant</NavLink>
+    <nav className="nav">
+        <NavLink to="/todo" style={{ marginRight: 10 }}>Tâches</NavLink>
+        <NavLink to="/minuteur" style={{ marginRight: 10 }}>Minuteur</NavLink>
+        <NavLink to="/statistiques" style={{ marginRight: 10 }}>Statistiques</NavLink>
+        <NavLink to="/conseils" style={{ marginRight: 10 }}>Conseils</NavLink>
+        <NavLink to="/notesrapides" style={{ marginRight: 10 }}>Notes rapides</NavLink>
+        <NavLink to="/assistant" style={{ marginRight: 10 }}>Assistant</NavLink>
     </nav>
   )
 }
